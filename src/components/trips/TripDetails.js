a4b2f3bc5699f860a35ceafe91470b230c3934aa
Eric Mauldin <emauldin84@gmail.com>
2019-05-16 21:53:48 -0400

    adds photo slider to trip details page

--- src/components/trips/TripDetails.js
+++ src/components/trips/TripDetails.js
@@ -1,6 +1,8 @@
 import React from 'react'
+import Slider from './Slider'
 
 export default function TripDetails() {
+    
     return (
         <div className='container trip-details'>
             <div className='card z-depth-0'>
@@ -11,27 +13,10 @@ export default function TripDetails() {
                 <div className='card-action grey lighten-4 grey-text'>
                     <div>12/25/2019</div>
                 </div>
-                <div className="slider">
-                    <ul className="slides">
-                        <li>
-                            <img src="https://dak95nwic4sny.cloudfront.net/73/south-africa-43313807-1533658538-ImageGalleryLightboxLarge.jpg"/>
-                        </li>
-                        <li>
-                            <img src="https://www.shipit.co.uk/blog/wp-content/uploads/cape-town-south-africa.jpg"/>
-                        </li>
-                        <li>
-                            <img src="https://www.azamaraclubcruises.com/sites/default/files/heros/cape-town-south-africa-1800x1000_1.jpg"/>
-                        </li>
-                        <li>
-                            <img src="https://www.usnews.com/dims4/USNEWS/68c2a5f/2147483647/crop/2560x1198%2B0%2B0/resize/1000x468/quality/85/?url=http%3A%2F%2Fcom-usnews-beam-media.s3.amazonaws.com%2F33%2F13%2F50e9178949f7be648612279ce00a%2Fbc16-south-africa-crop-editorial.jpg"/>
-                        </li>
-                    </ul>
-                </div>
+                <Slider />
+                
             </div>
         </div>
     )
-        // document.addEventListener('DOMContentLoaded', function() {
-        //     var elems = document.querySelectorAll('.slider');
-        //     var instances = M.Slider.init(elems, options);
-        //   });
+    
 }
