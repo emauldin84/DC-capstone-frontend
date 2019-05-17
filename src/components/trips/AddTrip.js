5080164934570bcf7718bfdf6240c4e458a603c2
Eric Mauldin <emauldin84@gmail.com>
2019-05-17 14:07:30 -0400

    requires location and date when adding new trip

--- src/components/trips/AddTrip.js
+++ src/components/trips/AddTrip.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import 'materialize-css/dist/css/materialize.min.css'
 
 // import M, {options} from 'materialize-css'
-import {DatePicker, options} from 'react-materialize'
+import {DatePicker} from 'react-materialize'
 
 export default class AddTrip extends Component {
     state = {
@@ -48,12 +48,13 @@ export default class AddTrip extends Component {
                 <h5 className='grey-text text-darken-3'>Add a New Trip</h5>
                 <div className='input-field'>
                     <label htmlFor='location'>Location</label>
-                    <input type='text' id='location' onChange={this.handleChange} />
+                    <input type='text' id='location' required onChange={this.handleChange} />
                 </div>
                 <div className='input-field'>
                     <label htmlFor='date'>Date</label>
+                    <DatePicker type='text' id='date' className='datepicker' required onChange={this.handleDateChange}/>
+                    
                     {/* <input type='text' className='datepicker' id='date' onChange={this.handleDateChange} /> */}
-                    <DatePicker id='date' name='date' onChange={this.handleDateChange}/>
                 </div>
                 <div className='input-field'>
                     <button className='btn purple lighten-1 z-depth-0'>Submit</button>
