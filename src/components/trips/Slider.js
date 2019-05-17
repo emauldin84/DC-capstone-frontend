80fe6bdbcc8bf1f37b6f3f5f0435e4c8051871da
Eric Mauldin <emauldin84@gmail.com>
2019-05-16 22:10:31 -0400

    displays trip details id from props.match.params in the trip details

--- src/components/trips/Slider.js
+++ src/components/trips/Slider.js
@@ -8,7 +8,7 @@ export default class Sliders extends Component {
         document.addEventListener('DOMContentLoaded', function() {
             var elems = document.querySelectorAll('.slider');
             var instances = M.Slider.init(elems, options);
-          });
+        });
 
     }
     render() {
--- src/components/trips/TripDetails.js
+++ src/components/trips/TripDetails.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import Slider from './Slider'
 
-export default function TripDetails() {
+export default function TripDetails(props) {
+
+    const id = props.match.params.id
     
     return (
         <div className='container trip-details'>
             <div className='card z-depth-0'>
                 <div className='card-content'>
-                    <span className='card-title'>Cape Town, South Africa</span>
-                    <p>Lorem ipsum aaand away we go</p>
+                    <span className='card-title'>Cape Town, South Africa - id:{id}</span>
+                    <p>Lorem ipsum aaand awaaaaaaay we go</p>
                 </div>
                 <div className='card-action grey lighten-4 grey-text'>
                     <div>12/25/2019</div>
