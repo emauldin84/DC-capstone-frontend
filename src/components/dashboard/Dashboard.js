87ae675b83c380d9e4c46fa880420e0c30d749df
Eric Mauldin <emauldin84@gmail.com>
2019-05-20 11:12:48 -0400

    adds button to 'add trip' styling

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -7,10 +7,10 @@ export default class Dashboard extends Component {
     render() {
         const { trips } = this.props
         return (
-        <div className='dashboard'>
+        <div className='dashboard section'>
             <div className='row'>
                 <div className='col s3 m2'>
-                    <NavLink  className='addTrip' to='/addtrip'>+ Trip</NavLink>
+                    <NavLink  className='addTrip btn-floating waves-effect waves-light' to='/addtrip' title='add trip'><i class="material-icons">add</i></NavLink>
                     <TripList trips={trips}/>
                 </div>
                 <div className='col s6 m10'>
