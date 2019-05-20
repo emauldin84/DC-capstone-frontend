24fa81e010c2410969b51db7c05c95f83a0b209b
Eric Mauldin <emauldin84@gmail.com>
2019-05-20 11:13:24 -0400

    removes map test text

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -14,25 +14,8 @@ export default class Dashboard extends Component {
                     <TripList trips={trips}/>
                 </div>
                 <div className='col s6 m10'>
-                    {/* <Map /> */}
-                    {/* <Geocoder
-                        accessToken='pk.eyJ1IjoiZW1hdWxkaW4iLCJhIjoiY2p2dmdhdjExMWMzMDQ5bDlwdzl0b2p1ZSJ9.HBj_nqaAQpYjoZx5vHOLOg'
-                        onSelect=required function
-                        onSuggest=optional function
-                        source=optional string, default 'mapbox.places'
-                        endpoint=optional string, default 'http://api.tiles.mapbox.com'
-                        inputClass=optional string, default ''
-                        inputPlaceholder=optional string, default 'Search'
-                        resultClass=optional string, default ''
-                        resultsClass=optional string, default ''
-                        showLoader=optional string, default ''
-                        inputPosition=optional string, default 'top', can be 'bottom'
-                        resultFocusClass=optional string, default 'strong'
-                        proximity=optional string, default '',
-                        bbox=optional string, default '',
-                        types=optional string, default '',
-                        focusOnMount=optional bool, default true
-    /> */}
+                    <Map />
+                    
                 </div>
             </div>
 
