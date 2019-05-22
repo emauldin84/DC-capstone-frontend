82ef4717bfa78cdba2cecf9bcafcd95b5973dfad
Jonathan Ray <jonathanray17@gmail.com>
2019-05-21 20:18:25 -0400

    moves App component into a parent component of LandingPage, forces users to login

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import axios from 'axios';
+import SignIn from './components/auth/SignIn';
+import App from './App';
 axios.defaults.withCredentials = true;
 
 
@@ -7,23 +9,51 @@ class RealApp extends React.Component{
   constructor(props){
     super(props)
     this.state = {
-
-    }
+      user : {},
+    };
     this._getRandomBackground();
   }
   componentDidMount(){
     
   }
   render(){
+    console.log(this.state.user);
+    console.log(typeof this.state.user);
     return(
-    <div>
-
-    </div>
+    this.state.user.id ?
+      <App user={this.state.user} />
+    :
+      <div style={{
+        display:"flex",
+        justifyContent:"center",
+        alignItems:"center",
+      }}>
+        <div style={{
+          marginTop:"-20vh",
+          paddingLeft:"5vw",
+        }}>
+          <h2>
+            Map Travel App
+          </h2>
+        </div>
+        <div style={{
+          width:"60%", 
+          height:"50%", 
+          position:"relative", 
+          float:"right", 
+          paddingTop:"20vh"
+        }}>
+          <SignIn handleSignIn={this._signIn} />
+        </div>
+      </div>
     )
   }
   _getRandomBackground = async () => {
     const randomNumber = Math.floor(Math.random() * Math.floor(3)) // gets random number, 1 - 3
     document.body.style.backgroundImage = `url("./assets/desktop_${randomNumber}.png")`;
+  }
+  _signIn = (user) => {
+    this.setState({user})
   }
 }
 export default RealApp;
--- src/components/auth/SignIn.js
+++ src/components/auth/SignIn.js
@@ -1,36 +1,28 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import axios from 'axios';
+axios.defaults.withCredentials = true;
+
 
 export default class SignIn extends Component {
-    state = {
-        email: '',
-        password: '',
+    constructor(props){
+        super(props);
+        this.state = {
+            email: '',
+            password: '',
+        };
     }
-
-    handleChange = (e) => {
-        this.setState({
-            [e.target.id]: e.target.value,
-        })
-    }
-
-    handleSubmit = (e) => {
-        e.preventDefault()
-        console.log(this.state)
-        
-        
-    }
-
     render() {
         return (
         <div className='container'>
-            <form onSubmit={this.handleSubmit} className='white'>
+            <form onSubmit={this._handleSubmit} className='white'>
                 <h5 className='grey-text text-darken-3'>Sign In</h5>
                 <div className='input-field'>
                     <label htmlFor='email'>Email</label>
-                    <input type='email' id='email' onChange={this.handleChange} />
+                    <input type='email' id='email' onChange={this._handleChange} />
                 </div>
                 <div className='input-field'>
                     <label htmlFor='password'>Password</label>
-                    <input type='password' id='password' onChange={this.handleChange} />
+                    <input type='password' id='password' onChange={this._handleChange} />
                 </div>
                 <div className='input-field'>
                     <button className='btn teal lighten-1 z-depth-0'>Login</button>
@@ -39,5 +31,20 @@ export default class SignIn extends Component {
             
         </div>
         )
+    }
+    _handleChange = (e) => {
+        this.setState({
+            [e.target.id]: e.target.value,
+        })
+    }
+    _handleSubmit = async (e) => {
+        e.preventDefault();
+        const signInCheck = await axios.post('/signin', this.state)
+            if (signInCheck.status !== 200){
+                console.log("Failed");
+            }
+            else{
+                this.props.handleSignIn(signInCheck.data)
+            }
     }
 }
