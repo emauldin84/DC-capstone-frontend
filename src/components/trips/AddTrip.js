a91bd174e0a485cecc20f4610f26eea4ce7daa4a
Jonathan Ray <jonathanray17@gmail.com>
2019-05-23 12:07:33 -0400

    adds autosuggesting feature with data from geocoder

--- src/components/trips/AddTrip.js
+++ src/components/trips/AddTrip.js
@@ -1,20 +1,26 @@
-import React, { Component } from 'react'
-import 'materialize-css/dist/css/materialize.min.css'
-import M, {options, elem} from 'materialize-css'
-import axios from 'axios'
+import React, { Component } from 'react';
+import 'materialize-css/dist/css/materialize.min.css';
+import M, {options, elem} from 'materialize-css';
+import axios from 'axios';
+import Autosuggest from 'react-autosuggest';
 
 
-import {DatePicker, Autocomplete}from 'react-materialize'
+import {DatePicker, Autocomplete}from 'react-materialize';
 
 export default class AddTrip extends Component {
-    state = {
-        location: 'Madison',
-        date: null,
-        tripdetails: '',
-        response: null,
-        fileName:null,
-        tripId:null,
-        message:null,
+    constructor(props){
+        super(props);
+        this.state = {
+            location: '',
+            date: null,
+            tripdetails: '',
+            response: [],
+            fileName:null,
+            tripId:null,
+            message:null,
+            value: '',
+            suggestions: [],
+        };
     }
 
     handleChange = (e) => {
@@ -55,7 +61,8 @@ export default class AddTrip extends Component {
         console.log('after axios', data)
 
         this.setState({
-            response: data.data.features
+            response: data.data.features,
+            suggestions : data.data.features,
         }, () => {
             this.state.response.forEach(res => {
                 console.log('response state',res.place_name)
@@ -64,36 +71,88 @@ export default class AddTrip extends Component {
         )
     }
 
+    getSuggestionValue = (suggestion) => suggestion;
+
+    
+    // This will allows the component to be a controlled component
+    updateAutosuggestField = (event, { newValue }) => {
+        this.setState({
+            value: newValue,
+            location: newValue,
+        }, ()=>{this.geocodeSearch()});
+        
+    };
+    
+    // Autosuggest will call these function every time you need to update suggestions.
+        getSuggestions = (value) => {
+            const inputValue = value.trim().toLowerCase();
+            const inputLength = inputValue.length;
+            const suggestionArryOfObjects =  inputLength === 0 ? [] : (this.state.response.length > 0 ? (this.state.response.filter(lang =>{
+                return lang.place_name.toLowerCase().slice(0, inputLength) === inputValue
+            })) : []);
+            return suggestionArryOfObjects.map(suggestion => suggestion.place_name)
+        };
+        // You already implemented this logic above, so just use it.
+        onSuggestionsFetchRequested = ({ value }) => {
+            this.setState({
+                suggestions: this.getSuggestions(value)
+            },
+            ()=>{
+                console.log("Here are your suggestions: ")
+                console.log(this.state.suggestions)
+            });
+        };
+    
+    // Autosuggest will call this function every time you need to clear suggestions.
+    // Housekeeping
+    onSuggestionsClearRequested = () => {
+        this.setState({
+            suggestions: []
+        });
+    };
+
     render() {
-        let data1 = []
-        if (this.state.response){
-            (this.state.response).forEach(location => {
-                // console.log(typeof location.place_name)
-                const name = location.place_name.split(',')[0]
-                console.log(name)
-                // name.toString();
-                data1[name.toString()] = null})
+        // let data1 = []
+        // if (this.state.response){
+        //     (this.state.response).forEach(location => {
+        //         // console.log(typeof location.place_name)
+        //         const name = location.place_name.split(',')[0]
+        //         console.log(name)
+        //         // name.toString();
+        //         data1[name.toString()] = null})
+        // }
+
+        const { value, suggestions } = this.state; // a little destructuring for conveinence 
+        const inputProps = {
+            placeholder: 'Type a programming language', // "Choose a destination"
+            value, // this.state.value aka what's in the input box right now
+            onChange: this.updateAutosuggestField
+        };
+        const renderSuggestion = suggestion => {
+            console.log("from renderSuggestion:");
+            console.log(suggestion)
+            return(
+            <div>
+                {suggestion.place_name}
+            </div>
+            )
         }
 
         return (
         <div className='container'>
             <form onSubmit={this.handleSubmit} id="myform" className='white' method='post' encType='multipart/form-data' action='/upload'>
                 <h5 className='grey-text text-darken-3'>Add a New Trip</h5>
-
-                {/* location selector field */}
                 
-                    <Autocomplete
-                        onChange={this.handleLocation}
-                        data={data1}
-                        >
-                        <div className='input-field'>
-                            <label htmlFor='location'>Location</label>
-                            <input type='text' id='location' className='autocomplete' required  />
-                        </div>
-                    </Autocomplete>
-                
+                <Autosuggest 
+                    suggestions={suggestions} // this.state.suggestions to select from
+                    onSuggestionsFetchRequested={this.onSuggestionsFetchRequested} // where Axios and the filtering happens
+                    onSuggestionsClearRequested={this.onSuggestionsClearRequested} // onBlur(-ish), clears the rendered suggestions
+                    getSuggestionValue={this.getSuggestionValue} // selector for suggestion, drops into state of final value
+                    renderSuggestion={renderSuggestion} // the div of suggestion below input field
+                    inputProps={inputProps} // placeholder, final value, and the onChange function
+                />
 
-                {/* date picker */}
+
                 <div className='input-field'>
                     <label htmlFor='date'>Date</label>
                     <DatePicker type='text' id='date' className='datepicker' required onChange={this.handleDateChange}/>
