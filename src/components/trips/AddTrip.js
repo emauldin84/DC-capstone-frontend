7b3d401f40e6407c4e27a62f5b64013e703a7c8a
Eric Mauldin <emauldin84@gmail.com>
2019-05-24 11:51:18 -0400

    fixes datepicker label and styling for new trip modal

--- src/components/trips/AddTrip.js
+++ src/components/trips/AddTrip.js
@@ -171,8 +171,8 @@ export default class AddTrip extends Component {
                             focusInputOnSuggestionClick={false} // when you take a suggestion, the input blurs
                         />
                         <div className='input-field'>
-                            <label htmlFor='date'>Date</label>
-                            <DatePicker type='text' id='date' className='datepicker' required onChange={this.handleDateChange}/>
+                            <label htmlFor='date' ></label>
+                            <DatePicker type='text' id='date' placeholder='Date' className='datepicker' required onChange={this.handleDateChange}/>
                         </div>
 
                         {/* text input field */}
