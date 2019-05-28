ef73ca2240879f29ead5a946fcf9bbe4232d39af
Jonathan Ray <jonathanray17@gmail.com>
2019-05-28 15:15:40 -0400

    adds ability to add and view multiple photos from TripDetails, add multiphotos from AddTrip

--- src/components/trips/TripDetails.js
+++ src/components/trips/TripDetails.js
@@ -4,6 +4,7 @@ import { Modal, Slider, Slide, Caption, } from 'react-materialize';
 import axios from 'axios';
 import M from 'materialize-css';
 import moment from 'moment';
+import Dropzone, {useDropzone} from "react-dropzone";
 
 
 export default class TripDetails extends React.Component{
@@ -14,42 +15,45 @@ export default class TripDetails extends React.Component{
             date : this.props.date,
             details : this.props.details,
             editPhotos : false, // changing the photos?
-            photos : [], // props.photos is returning an object, need to format it as an array to play nice with slides
+            photos : this.props.photos, // props.photos is returning an object, need to format it as an array to play nice with slides
             lat : this.props.lat,
             lon : this.props.lon,
             deleteThisTrip : false,
+            files: [],
         };
         this._updateDate.bind(this);
     }
     componentDidMount(){
         M.Datepicker.init(document.getElementById(`editTripDate${this.props.id}`), {autoClose:true, onSelect:this._updateDate});
-        this._getPhotos();
+        // this._getPhotos();
     }
     componentWillUnmount(){
+        // console.log("TripDetails, signing off...")
         // const elem = document.getElementById(`editTripDate${this.props.id}`);
         // const instance = M.Datepicker.getInstance(elem);
         // instance.destroy();
     }
     render(){
-        let {id, name, date, details, lat, lon} = this.props;
-        const {photos} = this.state;
-        // console.log(photos);
+        const { files, photos, } = this.state;
+        let {id, name, date, details, lat, lon, } = this.props;
+        let photosArray = null;
+        if(photos){
+            if(photos.length > 0){
+            photosArray = photos.map(photo => (photo));
+            }
+        }
+        console.log(photos)
         const options = {onCloseStart : ()=>{this._saveChanges();}, onOpenEnd : ()=> {console.log(this.state.name, id)}};
         date = moment(date).format("MMM Do YYYY");
-        const slides = photos.forEach(photo => {
-            return(
-                <Slide image={<img src={`photos/${photo.photo_url}`} key={photo.id} alt='' />}>
-                    <Caption>
-                        <h3>
-                        This is our big Tagline!
-                        </h3>
-                        <h5 className="light grey-text text-lighten-3">
-                        Here's our small slogan.
-                        </h5>
-                    </Caption>
-                </Slide>
-            )
-        })
+        let slides = null;
+        if(photosArray){
+            slides = photosArray.map((photo, i) => {
+                return(
+                    <Slide key={i} image={<img src={`photos/${photo}`}  alt='' />}>
+                    </Slide>
+                )
+            })
+        }
         return (
             <Modal id={`${id}`} open={true} options={options}>
                 <div className="modal-content">
@@ -61,22 +65,49 @@ export default class TripDetails extends React.Component{
                         <input type="text" id={`editTripDate${id}`} className="datepicker" defaultValue={date} ></input>
                     </div>
                     <p onBlur={(e)=>{this._updateDetails(e.target.textContent);}} contentEditable={true} suppressContentEditableWarning={true} >{details}</p>
-
                 </div>
-                {this.state.photos.length === 0 ?
-                    // would be nice to add a tool tip here
-                    <a title='Upload your trip photos!' id="addTripPhotos" className="pulse btn-floating waves-effect waves-light" onClick={this._choosePicture} onMouseOver={this._removePulse} >
+
+                    {/* <a title='Upload your trip photos!' id="addTripPhotos" className="pulse btn-floating waves-effect waves-light" onClick={this._choosePicture} onMouseOver={this._removePulse} >
                         <i className="pulse material-icons" >add</i>
                         <input id="tripPhotoInput" style={{visibility:"hidden"}} type="file" onChange={this._changeFileName} accept="image/png, image/jpeg, image/jpg, image/gif" />
-                    </a>
-                    :
-                    <>
-                    <Slider>
-                        {slides}
-                    </Slider>
-                    <img src="photos/1_pillows_1558981007810.jpeg"></img>
-                    </>
-                }
+                    </a> */}
+                    <Dropzone 
+                                onDrop={this._onDrop} 
+                                accept="image/*"   
+                                minSize={0} 
+                                maxSize={5242880} 
+                                multiple
+                            >
+                                {({
+                                getRootProps, 
+                                getInputProps, 
+                                isDragActive, 
+                                isDragReject, 
+                                isFileTooLarge,
+                                }) => (
+                                    <section className="container">
+                                        <div {...getRootProps()}>
+                                            <div className="btn">
+                                                <span>Images</span>
+                                            </div>
+                                            <input {...getInputProps()} />
+                                            {files.length > 0 ? <ul>{files.map(file=><li>{file.key}</li>)}</ul> : null}
+                                            {files.length === 0 && !isDragActive && `Upload photos for your trip!`}
+                                            {files.length === 0 && isDragActive && !isDragReject && "Drop it like it's hot!"}
+                                            {files.length === 0 && isDragReject && "File type not accepted, sorry!"}
+                                            {files.length === 0 && isFileTooLarge && (
+                                                <div className="text-danger mt-2">
+                                                    File is too large.
+                                                </div>
+                                            )}
+                                        </div>
+                                    </section>
+                                )}
+                            </Dropzone>
+                        <br />
+                        <br />
+                    {photosArray ? <Slider>{slides}</Slider> : null}
+
                 <div className="btn" onClick={this._toggleDeleteTrip}>
                     {this.state.deleteThisTrip? `Undo`:`Delete Trip`}
                 </div>
@@ -89,7 +120,7 @@ export default class TripDetails extends React.Component{
         }, () => {
             // we need to POST to db as well as alert the Dashboard 
             // component that it's time to freshly render with the latest from DB
-            const {name, details, photos, lat, lon} = this.state
+            const {name, details, lat, lon} = this.state
             let date1 = document.getElementById(`editTripDate${this.props.id}`).value.toString() 
             const date = moment(date1, 'MMM Do YYYY').format("YYYY-MM-DD")
             const propsDate = moment(this.props.date).format("YYYY-MM-DD")
@@ -99,18 +130,17 @@ export default class TripDetails extends React.Component{
                 lat,
                 lon,
                 trip_details : details,
-                trip_photos : photos,
             }
             if(this.state.deleteThisTrip){
                 axios.delete(`trips/delete/${this.props.id}`)
                 .then(this.props.updateApp)
             }
-            if((name!==this.props.name)||(date!==propsDate)||(details!==this.props.details)||(photos!==this.props.photos)){
+            if((name!==this.props.name)||(date!==propsDate)||(details!==this.props.details)){
                 console.log("prop id: ", this.props.id);
                 axios.post(`/trips/edit/${this.props.id}`, body)
-                // .then(({r}) => {console.log(destination)})
                 .then(this.props.updateApp)
             }
+
             this.props.shutTheDoorBehindYou();
         })
         
@@ -163,35 +193,53 @@ export default class TripDetails extends React.Component{
     _choosePicture = () => {
         document.getElementById("tripPhotoInput").click()
     }
-    _changeFileName = (e) => {
-        console.log(" ******** ********** ********** _changeFileName firing");
-        // If we are to implement multiple files per upload, we will have to change the logic to a forEach or Map.
-        console.log(e.target.files[0]);
-        if(e.target.files[0]){
-          this.setState({
-            fileName:e.target.files[0]
-          },() => {this._uploadFile(this.state.fileName)})
-        }
-      }
-      _uploadFile = (file) => {
-        let formData = new FormData();
-        formData.append('file',file)
-        const photoFormData = formData
-        this.setState({
-            photoFormData
-        }, async () => {
-          console.log(photoFormData);
-          
-          photoFormData.append('tripId', this.props.id)
-          const {data} = await axios.post('/photos', photoFormData, {headers:{'content-type':'multipart/form-data'}})
-          const latestPhotoURL = data.photo_url
-          this.setState({photos:[...this.state.photos, latestPhotoURL]});
+    // _changeFileName = (e) => {
+    //     console.log(" ******** ********** ********** _changeFileName firing");
+    //     // If we are to implement multiple files per upload, we will have to change the logic to a forEach or Map.
+    //     console.log(e.target.files[0]);
+    //     if(e.target.files[0]){
+    //         this.setState({
+    //             fileName:e.target.files[0]
+    //         },() => {this._uploadFile(this.state.fileName)})
+    //     }
+    // }
+    // _uploadFile = (file) => {
+    //     let formData = new FormData();
+    //     formData.append('file',file)
+    //     const photoFormData = formData
+    //     this.setState({
+    //         photoFormData
+    //     }, async () => {
+    //         console.log(photoFormData);
+            
+    //         photoFormData.append('tripId', this.props.id)
+    //         const {data} = await axios.post('/photos', photoFormData, {headers:{'content-type':'multipart/form-data'}})
+    //         const latestPhotoURL = data.photo_url
+    //         this.setState({photos:[...this.state.photos, latestPhotoURL]});
 
-        //   const {data} = await axios.post('/users/profilepic', this.state.photoFormData, {headers:{'content-type':'multipart/form-data'}} )
-        //   const latestPhotoURL = data.newPic[0].photo_url
-        //   this.setState({latestPhotoURL, tooltipShouldShow:true,})
+    //     //   const {data} = await axios.post('/users/profilepic', this.state.photoFormData, {headers:{'content-type':'multipart/form-data'}} )
+    //     //   const latestPhotoURL = data.newPic[0].photo_url
+    //     //   this.setState({latestPhotoURL, tooltipShouldShow:true,})
+    //     })
+    // }
+
+    _onDrop = async (files) => {
+        console.log(files);
+        let photoFormData = null;
+        photoFormData = new FormData();
+        files.forEach((file, i) => {
+            photoFormData.append(`file${i}`,file);
         })
-      }
+        console.log('tripId', this.props.id)
+        photoFormData.append('tripId', this.props.id);
+        console.log("photoFormData: ", photoFormData)
+        const {data} = await axios.post('/photos', photoFormData, {headers:{'content-type':'multipart/form-data'}});
+        const photos = data.trip_photos;
+        // const photos = photosStringObject.map(photo => JSON.parse(photo))
+        console.log("LOOK HERE");
+        console.log(photos);
+        console.log("*********************************************");
+        this.setState({photos})
+        // probably have to get a fresh trip object to get the latest photo references...
+    }
 }
-
-
