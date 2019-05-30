d64b3bde15142b5ec2b8972676515d14295d721c
Eric Mauldin <emauldin84@gmail.com>
2019-05-30 09:33:33 -0400

    adds border radius to modals and signin div

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -55,10 +55,7 @@ class LandingPage extends React.Component{
       </div>
     )
   }
-  _getRandomBackground = async () => {
-    const randomNumber = Math.floor(Math.random() * Math.floor(3, 20)) // gets random number, 0 - 19
-    document.body.style.backgroundImage = `url("./assets/desktop_${randomNumber}.jpg")`;
-  }
+  
   _signIn = (user) => {
     this.setState({user})
   }
