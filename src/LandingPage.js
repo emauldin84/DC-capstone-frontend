641d63d8b5a7f0ba0b8bda3448e37d02ba32cb20
Eric Mauldin <emauldin84@gmail.com>
2020-03-12 15:58:28 -0400

    adds background color to trips list and fixes page reload to not show login page if user is logged in

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 import SignIn from './components/auth/SignIn';
 import App from './App';
 import Modal from 'react-materialize/lib/Modal';
+import Spinner from './components/layout/Spinner'
 axios.defaults.withCredentials = true;
 
 
@@ -12,10 +13,19 @@ class LandingPage extends React.Component{
     this.state = {
       user : {},
       modalShouldShow : true,
+      loading: true,
     };
+
     axios.get('/session')
-    .then(({data}) => this._signIn(data.user));
+    .then(({data}) => this._signIn(data.user))
+    .then(() => {
+      this.setState({
+        loading: false,
+      })
+    })
+
   }
+
   render(){
     if(this.state.user.id){
       document.body.style.backgroundImage = null;
@@ -24,6 +34,7 @@ class LandingPage extends React.Component{
     this.state.user.id ?
       <App user={this.state.user} handleSignOut={this._clearUser} updateLanding={this._updateUser} />
     :
+    this.state.loading ? <Spinner /> : 
       <div className='container right column'>
         <div className='landing-page-div'>
           <SignIn signInUser={this._signIn} showModal={this._showModal} hideModal={this._hideModal}/>
